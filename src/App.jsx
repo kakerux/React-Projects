9cc219e404a613cd4013f28c5da629497e19a9fc
nishant thakur <nishantt7049@gmail.com>
2026-04-14 11:44:31 +0530

    day1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+// import React from "react"
+
+// import Card from "./Card"
+import "./App.css"
+import ZClass1 from "./ZClass1"
+import Vite from '/vite.svg'
+
+
+
+const App=()=>{
+
+
+  return(
+
+    <>
+
+   
+     <h1>Hellow from react</h1>
+     <ZClass1/>
+     <img src={Vite}  width="200px" />
+    
+
+    </>
+
+      )
+  
+
+}
+
+
+
+export default App
---
+++ src/ZClass1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ZClass1 = () => {
+    const myName="Nishant"
+  return (
+    <>
+    
+    <h1>Hellow from another file {myName}</h1>
+    </>
+  )
+}
+
+export default ZClass1
